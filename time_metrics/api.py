4d54261dbc751b1cbacc984a9b449da89c5a9205
arthur <arthur@wallstreetweb.net>
2016-06-21 19:56:08 +0200

    Update api

--- time_metrics/api.py
+++ time_metrics/api.py
@@ -1,6 +1,20 @@
-from wsw_stats.utils import get_backend
+from .models import Metric, MetricItem
 
 
 def put_metric(slug, object=None, count=1, **kwargs):
-    backend = get_backend()
-    backend.put_metric(slug, object=object, count=count, **kwargs)
+    from django.conf import settings
+    from django.contrib.sites.models import Site
+
+    try:
+        metric = Metric.objects.get(slug=slug)
+    except Metric.DoesNotExist:
+        metric = Metric.objects.create(slug=slug, name=slug)
+
+    site = Site.objects.get(pk=settings.SITE_ID)
+
+    MetricItem.objects.create(
+        metric=metric,
+        content_object=object,
+        site=site,
+        count=count
+    )
